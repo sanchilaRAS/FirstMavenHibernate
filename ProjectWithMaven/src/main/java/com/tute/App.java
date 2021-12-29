44fcd8f5fcc488e04d08687e57f5a85bf1e28874
sanchilaRAS <sanchilaranaweera@gmail.com>
2021-12-29 17:19:01 +0530

    sesond commit

--- ProjectWithMaven/src/main/java/com/tute/App.java
+++ ProjectWithMaven/src/main/java/com/tute/App.java
@@ -1,5 +1,8 @@
 package com.tute;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Project Started!" );
+        //SessionFactory factory=new Configuration().configure().buildSessionFactory();
+        
+        Configuration cfg=new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory=cfg.buildSessionFactory();
+        
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
+       
     }
 }
